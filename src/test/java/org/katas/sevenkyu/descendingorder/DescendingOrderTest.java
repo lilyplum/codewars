7b67ff10597607a7318b5cd312b625bb780e0e32
Lily Chen <137886276+lilyplum@users.noreply.github.com>
2023-11-26 00:06:33 +0100

    update dependencies

--- src/test/java/org/katas/sevenkyu/descendingorder/DescendingOrderTest.java
+++ src/test/java/org/katas/sevenkyu/descendingorder/DescendingOrderTest.java
@@ -1,10 +1,9 @@
 package org.katas.sevenkyu.descendingorder;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class DescendingOrderTest {
 
