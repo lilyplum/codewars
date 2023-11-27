950577103921b7c20997063ca9fe408cc67b42b1
lilyplum <lily.code@outlook.com>
2023-11-27 11:31:13 +0100

    remove redundant code

--- src/main/java/org/katas/Main.java
+++ src/main/java/org/katas/Main.java
@@ -4,9 +4,5 @@ import org.katas.sixkyu.isprime.IsNumberPrime;
 
 public class Main {
     public static void main(String[] args) {
-        IsNumberPrime isNumberPrime = new IsNumberPrime();
-        boolean cond = isNumberPrime.isPrime(4);
-        if (cond) System.out.println("true");
-        else System.out.println("false");
     }
 }
